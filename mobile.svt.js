e397e996cf07017034c1c50d1da8ae3f8bd6ea04
GitTemporaryAccount <x9Shiro@gmail.com>
2025-05-19 21:28:15 +0200

    Update mobile.svt.js

--- mobile.svt.js
+++ mobile.svt.js
@@ -2,9 +2,10 @@ function modif(b,t){
     var lb=document.getElementsByClassName("btt");
     var lt=document.getElementsByClassName("para");
     for(var i=0;i<lb.length;i++){lb[i].className="bt";}
-    for(var i=0;i<lt.length;i++){lt[i].style.zIndex=-1;}
+    for(var i=0;i<lt.length;i++){lt[i].style.zIndex=-1;lt[i].style.opacity=0;}
     b.className="btt";
     t.style.zIndex=0;
+    t.style.opacity=1;
 }
 function toggleMenu(x,y) {
     x.classList.toggle("change");
